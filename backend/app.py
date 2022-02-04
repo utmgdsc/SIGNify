2299bae0a9ac5850ed84199bd8b73ec826149fa4
Mitesh Ghimire <mitesh.ghimire@mail.utoronto.ca>
2022-02-03 22:20:55 -0500

    [Sig-4] Created api for uploading video

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -6,3 +7,18 @@ app = Flask(__name__)
 def index():
     return "Hello, welcome to the api endpoint for SIGNify!"
 
+@app.route("/upload_video", methods = ["POST"])
+def upload_video():
+    """
+    Api to receive video and convert ASL to text reponse 
+    """
+    try:
+        response = convert_ASL(request)
+    except Exception:
+        response = {"error": 400, "message": "Error cannot convert video to text"}
+    return json.dumps(response)
+
+
+def convert_ASL(request):
+    # ML component to process video goes here
+    return {"word": "hello"}
