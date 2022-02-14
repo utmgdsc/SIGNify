35837c546a93d59f6a916fd4fbac7a4601643f13
Blizzard416 <Blizzard416@github.com>
2022-02-14 18:19:18 -0500

    [SIG-9] Finish updating setting page
    Need to modify setting page layout before starting to work on theme color

--- frontend/lib/settings.dart
+++ frontend/lib/settings.dart
@@ -20,137 +20,101 @@ class MySettingsPage extends StatelessWidget {
         title: const Text("Settings"),
       ),
       body: Container(
-        margin: const EdgeInsets.only(top: 50, left: 15),
+        margin: const EdgeInsets.only(top: 50),
         child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  const Icon(
-                    Icons.access_time,
-                    size: 24.0,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "History",
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ],
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextButton.icon(
+              icon: const Icon(
+                Icons.access_time,
+                size: 24.0,
               ),
-              Row(
-                children: <Widget>[
-                  const Icon(
-                    Icons.mode_night_outlined,
-                    size: 24.0,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Dark/Light Mode",
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ],
+              label: const Text(
+                "History",
+                style: TextStyle(fontWeight: FontWeight.w900),
               ),
-              Row(
-                children: <Widget>[
-                  const Icon(
-                    Icons.border_color_outlined,
-                    size: 24.0,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Theme Color",
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ],
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(left: 15),
+                  alignment: Alignment.centerLeft),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: const Icon(
+                Icons.mode_night_outlined,
+                size: 24.0,
               ),
-              Row(
-                children: <Widget>[
-                  const Icon(
-                    Icons.format_size_sharp,
-                    size: 24.0,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Text Size",
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ],
+              label: const Text(
+                "Dark/Light Mode",
+                style: TextStyle(fontWeight: FontWeight.w900),
               ),
-              Divider(
-                color: Colors.teal.shade100,
-                thickness: 1,
-                endIndent: 15,
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(left: 15),
+                  alignment: Alignment.centerLeft),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: const Icon(
+                Icons.border_color_outlined,
+                size: 24.0,
               ),
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.logout,
-                    color: theme[900],
-                    size: 24.0,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Log out",
-                        style: TextStyle(fontWeight: FontWeight.w900),
-                      ),
-                    ),
-                  ),
-                ],
+              label: const Text(
+                "Theme Color",
+                style: TextStyle(fontWeight: FontWeight.w900),
               ),
-            ]),
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(left: 15),
+                  alignment: Alignment.centerLeft),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: const Icon(
+                Icons.format_size_sharp,
+                size: 24.0,
+              ),
+              label: const Text(
+                "Text Size",
+                style: TextStyle(fontWeight: FontWeight.w900),
+              ),
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(left: 15),
+                  alignment: Alignment.centerLeft),
+              onPressed: () {},
+            ),
+            const Divider(
+              color: Colors.grey,
+              indent: 15,
+              endIndent: 15,
+            ),
+            TextButton.icon(
+              icon: const Icon(
+                Icons.logout,
+                size: 24.0,
+              ),
+              label: const Text(
+                "Log out",
+                style: TextStyle(fontWeight: FontWeight.w900),
+              ),
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(left: 15),
+                  alignment: Alignment.centerLeft),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
