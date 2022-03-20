f239f397d923122619aed3a8a4d8c9e762cfd223
Blizzard416 <1399507948@qq.com>
2022-03-20 13:46:03 -0400

    [SIG-35] Update screenshoot

--- frontend/lib/camera_screen.dart
+++ frontend/lib/camera_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:screenshot/screenshot.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/settings.dart';
-import 'package:tflite/tflite.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -14,20 +16,18 @@ class _CameraScreenState extends State<CameraScreen> {
   bool _recording = false;
   bool _initialized = true;
   late CameraController _controller;
-  late CameraImage cameraImage;
-  late List recognitions;
+  late Timer timer;
+  ScreenshotController screenshotController = ScreenshotController();
 
   @override
   void initState() {
     _cameraSetUp();
-    _loadModel();
     super.initState();
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    Tflite.close();
     super.dispose();
   }
 
@@ -42,35 +42,16 @@ class _CameraScreenState extends State<CameraScreen> {
   // Start or stop recording
   _recordVideo() async {
     if (_recording) {
-      _controller.stopImageStream();
+      timer.cancel();
       setState(() => _recording = false);
     } else {
       setState(() => _recording = true);
-      _controller.startImageStream((image) {
-        cameraImage = image;
-        _runModel();
+      timer = Timer.periodic(const Duration(milliseconds: 500), (timer) async {
+        screenshotController.capture().then((capturedImage) async {
+          File image = File.fromRawPath(capturedImage!);
+        });
       });
     }
-  }
-
-  Future _loadModel() async {
-    Tflite.close();
-    await Tflite.loadModel(model: "", labels: "");
-  }
-
-  _runModel() async {
-    recognitions = await Tflite.detectObjectOnFrame(
-          bytesList: cameraImage.planes.map((plane) {
-            return plane.bytes;
-          }).toList(),
-          imageHeight: cameraImage.height,
-          imageWidth: cameraImage.width,
-          imageMean: 127.5,
-          imageStd: 127.5,
-          numResultsPerClass: 1,
-          threshold: 0.1,
-        ) ??
-        [];
   }
 
   @override
@@ -80,42 +61,59 @@ class _CameraScreenState extends State<CameraScreen> {
         child: CircularProgressIndicator(),
       );
     } else {
-      return Scaffold(
-        body: Stack(
-          children: [
-            CameraPreview(_controller),
-            Align(
-              alignment: Alignment.topRight,
-              child: SafeArea(
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MySettingsPage()),
-                    );
-                  },
-                  icon: const Icon(
-                    Icons.settings,
-                    size: 30,
-                    color: Colors.black,
+      return Screenshot(
+        controller: screenshotController,
+        child: Scaffold(
+          body: Stack(
+            children: [
+              CameraPreview(_controller),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 5,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: double.infinity,
-                height: 100,
-                color: Colors.black54,
-                padding: const EdgeInsets.all(25),
-                child: FloatingActionButton(
-                  child: Icon(_recording ? Icons.stop : Icons.circle),
-                  onPressed: () => _recordVideo(),
+              Align(
+                alignment: Alignment.topRight,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MySettingsPage()),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.settings,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: Colors.black54,
+                  padding: const EdgeInsets.all(25),
+                  child: FloatingActionButton(
+                    child: Icon(_recording ? Icons.stop : Icons.circle),
+                    onPressed: () => _recordVideo(),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
     }
